3f9b1a87dcc8260ee0270783f885b92d2feb45a2
Bruno Henrique <brunohenriquesantos272@gmail.com>
2021-05-14 06:14:53 -0300

    refactor: adding TSyringe to inject dependencies

--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -1,4 +1,4 @@
-import { Category } from '../model/Category';
+import { Category } from '../entities/Category';
 
 interface ICreateCaregoryDTO {
   name: string;
@@ -6,9 +6,9 @@ interface ICreateCaregoryDTO {
 }
 
 interface ICategoriesRepository {
-  findByName(name: string): Category;
-  list(): Category[];
-  create({ name, description }: ICreateCaregoryDTO): void;
+  findByName(name: string): Promise<Category>;
+  list(): Promise<Category[]>;
+  create({ name, description }: ICreateCaregoryDTO): Promise<void>;
 }
 
 export { ICategoriesRepository, ICreateCaregoryDTO };
