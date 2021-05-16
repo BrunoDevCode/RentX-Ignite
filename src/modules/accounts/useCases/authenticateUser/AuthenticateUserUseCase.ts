d95981dcef3c7e048a7782d0061473e1222398ea
Bruno Henrique <brunohenriquesantos272@gmail.com>
2021-05-16 12:14:50 -0300

    feat: adding error handling

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import { inject, injectable } from 'tsyringe';
 
+import { AppError } from '../../../../errors/AppError';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
@@ -28,13 +29,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error('Email or password incorrect !');
+      throw new AppError('Email or password incorrect !');
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error('Email or password incorrect !');
+      throw new AppError('Email or password incorrect !');
     }
 
     const token = sign({}, 'c11b514ea321cf6c0b6e64fdd918086f', {
