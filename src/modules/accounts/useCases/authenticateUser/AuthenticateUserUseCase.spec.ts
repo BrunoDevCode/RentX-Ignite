e0ad90aeef2d102f9e7e263e3d96fbdda8ac83e1
Bruno Henrique <brunohenriquesantos272@gmail.com>
2021-08-26 21:31:03 -0300

    test: adding authenticate user test and mock

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,65 @@
+import { AppError } from '@errors/AppError';
+
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { UserRepositoryInMemory } from '../../repositories/in-memory/UsersRepositoryInMemory';
+import { CreateUserUseCase } from '../createUser/CreateUserUseCase';
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
+
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let usersRepositoryInMemory: UserRepositoryInMemory;
+let createUserUseCase: CreateUserUseCase;
+
+describe('Authenticate User', () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UserRepositoryInMemory();
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      usersRepositoryInMemory
+    );
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it('Should be able to authenticate an user', async () => {
+    const user: ICreateUserDTO = {
+      driver_license: 'HHM0132',
+      email: 'chamanalama@gmail.com',
+      password: 'chamahh',
+      name: 'Batman',
+    };
+
+    await createUserUseCase.execute(user);
+
+    const result = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(result).toHaveProperty('token');
+  });
+
+  it('Should not be able to authenticate an non existent user', () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({
+        email: 'false@mail.com',
+        password: 'notrue',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should not be able to authenticate with incorrect password', () => {
+    expect(async () => {
+      const user: ICreateUserDTO = {
+        driver_license: 'HHM0132',
+        email: 'chamanalama@gmail.com',
+        password: 'chamahh',
+        name: 'Batman',
+      };
+
+      await createUserUseCase.execute(user);
+
+      await authenticateUserUseCase.execute({
+        email: 'chamanalama@gmail.com',
+        password: 'wrongPass',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
